6752a5b42c772d89afd9921fd6807d8c39468b0b
“Ahmad <ahmadjunaidibeds@gmail.com>
2025-05-07 18:48:26 +0700

    - Mengganti menu yang ditulis secara manual menjadi dinamis dari main_menu_table - Meningkatkan skalabilitas untuk penambahan menu ke depan"

--- menu.py
+++ menu.py
@@ -1,11 +1,12 @@
+from database import main_menu_table
+
 def show_menu():
     print("=" * 40)
     print("     SISTEM PARKIR SEDERHANA")
     print("=" * 40)
-    print("1. Tambah Kendaraan")
-    print("2. List Kendaraan")
-    print("3. Keluar Parkir")
-    print("4. Laporan Pendapatan")
-    print("5. Membership")
-    print("6. Exit")
+    
+    # Menggunakan tabel untuk menampilkan menu
+    for key, item in sorted(main_menu_table.items()):
+        print(f"{key}. {item['label']}")
+    
     print("=" * 40)
